b98359ca150e015594768baf9851df590c9f6ab3
Jonathan Bruenderman <jonbruenderman@hotmail.com>
2024-10-11 21:22:53 -0700

    deploy

--- next.config.js
+++ next.config.js
@@ -2,11 +2,3 @@
 const nextConfig = {}
 
 module.exports = nextConfig
-
-nodejs_bundling_options = NodejsBundlingOptions(esbuild_version="0.21.5")
-
-some_lambda = NodejsFunction(
-    scope=self,
-    ...
-    bundling=nodejs_bundling_options,
-)
