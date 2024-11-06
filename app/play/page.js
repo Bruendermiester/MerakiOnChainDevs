f884298aa12a5c90061a276d0a1ddabaf4d23e37
Jonathan Bruenderman <jonbruenderman@hotmail.com>
2024-11-06 06:17:12 -0800

    testing play

---
+++ app/play/page.js
@@ -0,0 +1,33 @@
+'use client';
+import styles from '../page.module.css'
+
+export default function Home() {
+  return (
+<>
+    <header>
+        <img
+        className={styles.logo}
+        src='/images/Logo3.png'
+        alt="Logo"
+        />
+        
+    </header>
+    <main className={styles.mainPlay}>
+      <h2>Meraki OnChain Devs</h2>
+      <div className={styles.projects}>
+        <iframe
+          src="/unity.html"
+          width="100%"
+          height="600px"
+          title="Unity Game"
+          frameBorder="0"
+        ></iframe>
+      </div>
+
+      <div>
+        {/* <Subscribe></Subscribe> */}
+      </div>
+    </main>
+    </>
+  )
+}
